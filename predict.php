d0f3fffa57106227ba72c9aaef0a80cb6f8c363a
Chaitanya19 <Chaitanya.vaddula001@umb.edu>
2015-07-24 15:03:16 -0400

    Backend php script

---
+++ predict.php
@@ -0,0 +1,13 @@
+<?php
+
+$city = $_GET['city'];
+
+$city = str_replace(" ", "", $city);
+
+$data = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+
+preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</i', $data, $matches);
+
+echo($matches[1]);
+
+?>
